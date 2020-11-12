f4e9377d1f3f143a2001b9a46e5af089cb9def01
BHung <bhung2011@gmail.com>
2020-11-12 11:53:28 +0800

    R G B to be recognized as different objects

--- LabelHelper.py
+++ LabelHelper.py
@@ -5,26 +5,41 @@ from os import stat as os_stat
 import json
 import configparser
 import contovia
+import numpy as np
 
 
 def get_contours(filepath: str, threshold: int, color_max: int, mode: int, preview: bool) -> list:
+    print(filepath)
     # 讀取圖檔
     im = cv2.imread(filepath)
     # 轉灰階
-    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    # 定門檻值 (灰階圖，門檻，最大值，)
-    ret, thresh = cv2.threshold(imgray, threshold, color_max, mode)
+    # im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    lows = []
+    highs = []
+    lows.append(np.array([128, 0, 0]))
+    lows.append(np.array([0, 128, 0]))
+    lows.append(np.array([0, 0, 128]))
+    highs.append(np.array([255, 0, 0]))
+    highs.append(np.array([0, 255, 0]))
+    highs.append(np.array([0, 0, 255]))
+    cons = []
+    for i in range(3):
+        output = cv2.inRange(im, lows[i], highs[i])
+        ret, thresh = cv2.threshold(output, threshold, color_max, mode)
 
-    # 4.Erodes the Thresholded Image
-    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-    thresh = cv2.erode(thresh, element)
+        # 4.Erodes the Thresholded Image
+        element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+        thresh = cv2.erode(thresh, element)
 
-    # 尋找輪廓
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # 尋找輪廓
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_L1)
+        if len(contours) != 0:
+            for area in contours:
+                cons.append(area)
 
-    if preview and (len(contours) != 0):
+    if preview and (len(cons) != 0):
         # 畫出輪廓線
-        img = cv2.drawContours(im, contours, -1, (0, 255, 0), 3)
+        img = cv2.drawContours(im, cons, -1, (0, 255, 0), 3)
         # 縮小四分之一比較容易看
         height, width = img.shape[0], img.shape[1]
         img_resize = cv2.resize(im, (int(width / 4), int(height / 4)))
@@ -32,7 +47,7 @@ def get_contours(filepath: str, threshold: int, color_max: int, mode: int, previ
         cv2.imshow('Image', img_resize)
         cv2.waitKey(0)
 
-    return contours
+    return cons
 
 
 if __name__ == '__main__':
@@ -61,13 +76,14 @@ if __name__ == '__main__':
         g_color_max = int(config.get('ContoursSettings', 'ColorMax'))
         g_mode = int(config.get('ContoursSettings', 'Mode'))
     except FileNotFoundError:
+
         config = configparser.ConfigParser()
 
-        config["GeneralSettings"] = {'ImageDir': '../images'  # 圖片資料夾
+        config["GeneralSettings"] = {'ImageDir': './images'  # 圖片資料夾
                                      }
         config["ContoursSettings"] = {'Threshold': '20',
                                       'ColorMax': '255',
-                                      'Mode': '1'
+                                      'Mode': '0'
                                       }
         folder = config.get('GeneralSettings', 'ImageDir')
         g_threshold = int(config.get('ContoursSettings', 'Threshold'))
@@ -82,7 +98,7 @@ if __name__ == '__main__':
     count = 0
 
     for filename in images:
-        if filename.endswith(".png") or filename.endswith("_mask.tif"):
+        if filename.endswith(".png"):
             path = folder + '/' + filename
             # 取得圖片 Contours
             CONTOURS = get_contours(path, g_threshold, g_color_max, g_mode, prev)
@@ -92,10 +108,7 @@ if __name__ == '__main__':
                 continue
 
             # 更改附檔名 (空拍照片皆為 jpg 格式)
-            if filename.endswith("_mask.tif"):
-                fnamejpg = filename[:len(filename) - 9] + '.JPG'
-            else:
-                fnamejpg = os_path.splitext(filename)[0] + '.JPG'
+            fnamejpg = os_path.splitext(filename)[0] + '.JPG'
             filesize = os_stat(folder + '/' + fnamejpg).st_size
             # 將 Contours 點寫入 JSON
             contovia.process_image(fnamejpg, filesize, CONTOURS, "vehicle", labelfile)
